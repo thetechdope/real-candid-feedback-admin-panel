a624a87e2ac46d272e493ba7ad73084a38889512
supriya-patil-ots <supriya.patil@otssolutions.com>
2023-02-08 19:58:14 +0530

    first

---
+++ client/src/components/data/BusinessesData.js
@@ -0,0 +1,47 @@
+const CustomersData = [
+    {
+      id: 1,
+      month: "Jan",
+      noOfCustomersGained: 256,
+      noOfCustomersLost: 76,
+    },
+    {
+      id: 2,
+      month: "Feb",
+      noOfCustomersGained: 616,
+      noOfCustomersLost: 23,
+    },
+    {
+      id: 3,
+      month: "Mar",
+      noOfCustomersGained: 365,
+      noOfCustomersLost: 50,
+    },
+    {
+      id: 4,
+      month: "Apr",
+      noOfCustomersGained: 800,
+      noOfCustomersLost: 36,
+    },
+    {
+      id: 5,
+      month: "May",
+      noOfCustomersGained: 475,
+      noOfCustomersLost: 401,
+    },
+    {
+      id: 6,
+      month: "Jun",
+      noOfCustomersGained: 345,
+      noOfCustomersLost: 787,
+    },
+    {
+      id: 7,
+      month: "Jul",
+      noOfCustomersGained: 600,
+      noOfCustomersLost: 10,
+    },
+  ];
+  
+  export default CustomersData;
+  
