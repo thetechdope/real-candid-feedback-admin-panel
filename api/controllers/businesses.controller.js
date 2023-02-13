4a408a49aa409bd0b627614a6e7a2860fae39bf0
unknown <sahilsingh70526@gmail.com>
2023-02-13 15:29:08 +0530

    minute change

--- api/controllers/businesses.controller.js
+++ api/controllers/businesses.controller.js
@@ -16,36 +16,40 @@ export const getBusinessDetailsByEmail = async (req, res) => {
 export const addNewBusiness = async (req, res) => {
 	const { businessName, businessAddress, businessEmail, password, businessPhoneNumber, businessWebsiteUrl } = req.body;
 
-    const encryptedPassword = await bcrypts.hash(password, 10);
+	const encryptedPassword = await bcrypts.hash(password, 10);
 
-		let newBusinessDetails = {
-			businessName: businessName,
-			businessAddress: businessAddress,
-			businessEmail: businessEmail,
-			password: encryptedPassword,
-			businessPhoneNumber: businessPhoneNumber,
-			businessWebsiteUrl: businessWebsiteUrl,
-			otp: Math.floor((Math.random() + 1) * 1000),
-		};
+	let newBusinessDetails = {
+		businessName: businessName,
+		businessAddress: businessAddress,
+		businessEmail: businessEmail,
+		password: encryptedPassword,
+		businessPhoneNumber: businessPhoneNumber,
+		businessWebsiteUrl: businessWebsiteUrl,
+		otp: Math.floor((Math.random() + 1) * 1000),
+	};
 
-		const addedBusiness = await BusinessModel.create(newBusinessDetails);
-		addedBusiness.save();
+	const addedBusiness = await BusinessModel.create(newBusinessDetails);
+	addedBusiness.save();
 
-    res.status(200);
-    res.json(addedBusiness);
+	res.status(200);
+	res.json(addedBusiness);
 
 };
 
 export const updateBusinessProfile = async (req, res) => {
 	const { email } = req.params;
-		const updateBusinessDetails = await BusinessModel.findOneAndUpdate(
-			{ businessEmail: email },
-			{ $set: { ...req.body } },
-			{ new: true }
-		);
-		if (!updateBusinessDetails) {
-			res.status(400).json({ message: "Business Profile Not found" });
-		} else {
-			res.json(updateBusinessDetails);
-		}
+	const updateBusinessDetails = await BusinessModel.findOneAndUpdate(
+		{ businessEmail: email },
+		{ $set: { ...req.body } },
+		{ new: true }
+	);
+	if (!updateBusinessDetails) {
+		res.status(400).json({ message: "Business Profile Not found" });
+		throw new Error("Business Profile Not found")
+	} else {
+		res.json({
+			data:updateBusinessDetails,
+			message:"Congrats your business account has been updated."
+		});
+	}
 };
