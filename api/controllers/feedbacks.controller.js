29c35e07932316caaafee76305d198a658b18929
TheTechDope <thetechdope.in@gmail.com>
2023-02-13 18:27:03 +0530

    Updated Customers & Businesses Components

--- api/controllers/feedbacks.controller.js
+++ api/controllers/feedbacks.controller.js
@@ -3,73 +3,74 @@ import CustomersModel from "../models/customers.model.js";
 import BusinessModel from "../models/businesses.model.js";
 
 export const getAllFeedbacks = async (req, res) => {
-	const response = await FeedbacksModel.find();
-	res.status(200).json(response);
+  const response = await FeedbacksModel.find();
+  res.status(200).json(response);
 };
 
 export const getCustomersFeedbacksByEmail = async (req, res) => {
-	const { email } = req.params;
-	const response = await FeedbacksModel.find({ customerEmail: email });
-	res.status(200).json(response);
+  const { email } = req.params;
+  const response = await FeedbacksModel.find({ customerEmail: email });
+  res.status(200).json(response);
 };
 
 export const getBusinessesFeedbacksByEmail = async (req, res) => {
-	const { email } = req.params;
-	const response = await FeedbacksModel.find({ businessEmail: email });
-	res.status(200).json(response);
+  const { email } = req.params;
+  const response = await FeedbacksModel.find({ businessEmail: email });
+  res.status(200).json(response);
 };
 
 export const addNewFeedback = async (req, res) => {
-	const { rating, feedback, customerEmail, businessEmail, isAnonymous } = req.body;
+  const { rating, feedback, customerEmail, businessEmail, isAnonymous } =
+    req.body;
 
-	const checkForCustomer = await CustomersModel.find({ email: customerEmail });
-	const checkForBusiness = await BusinessModel.find({
-		businessEmail: businessEmail,
-	});
-	console.log(checkForBusiness);
+  const checkForCustomer = await CustomersModel.find({ email: customerEmail });
+  const checkForBusiness = await BusinessModel.find({
+    businessEmail: businessEmail,
+  });
+  console.log(checkForBusiness);
 
-	//  ----------- for business registered -------------------------------
+  //  ----------- for business registered -------------------------------
 
-	if (checkForBusiness[0].isActive) {
-		// ------------ code for registered customer -----------------------------
-		if (checkForCustomer[0].isActive) {
-			if (!isAnonymous) {
-				const addedFeedback = await FeedbacksModel.create({
-					rating: rating,
-					feedback: feedback,
-					customerEmail: customerEmail,
-					businessEmail: businessEmail,
-					isAnonymous: isAnonymous,
-				});
-				addedFeedback.save();
-				res.status(200);
-				res.json({
-					data: addedFeedback,
-					message: `Congrats ${checkForCustomer[0].firstName} your feedback has been added.`,
-				});
-			} else {
-				const addedFeedback = await FeedbacksModel.create({
-					rating: rating,
-					feedback: feedback,
-					customerEmail: null,
-					businessEmail: businessEmail,
-					isAnonymous: true,
-				});
-				addedFeedback.save();
-				res.status(200);
-				res.json({
-					data: addedFeedback,
-					message: "Your feedback has been saved as anonymous",
-				});
-			}
-		} else {
-			res.status(400);
-			throw new Error(
-				"Your account has been blocked, you have not access to add feedback anymore,\n Please connect to admin for reactivating your account."
-			);
-		}
-	} else {
-		res.status(400);
-		throw new Error("Sorry this business account has been blocked by admin.");
-	}
+  if (checkForBusiness[0].isActive) {
+    // ------------ code for registered customer -----------------------------
+    if (checkForCustomer[0].isActive) {
+      if (!isAnonymous) {
+        const addedFeedback = await FeedbacksModel.create({
+          rating: rating,
+          feedback: feedback,
+          customerEmail: customerEmail,
+          businessEmail: businessEmail,
+          isAnonymous: isAnonymous,
+        });
+        addedFeedback.save();
+        res.status(200);
+        res.json({
+          data: addedFeedback,
+          message: `Congrats ${checkForCustomer[0].firstName} your feedback has been added.`,
+        });
+      } else {
+        const addedFeedback = await FeedbacksModel.create({
+          rating: rating,
+          feedback: feedback,
+          customerEmail: null,
+          businessEmail: businessEmail,
+          isAnonymous: true,
+        });
+        addedFeedback.save();
+        res.status(200);
+        res.json({
+          data: addedFeedback,
+          message: "Your feedback has been saved as anonymous",
+        });
+      }
+    } else {
+      res.status(400);
+      throw new Error(
+        "Your account has been blocked, you have not access to add feedback anymore,\n Please connect to admin for reactivating your account."
+      );
+    }
+  } else {
+    res.status(400);
+    throw new Error("Sorry this business account has been blocked by admin.");
+  }
 };
---
+++ client/src/components/Customers/CustomersComponent.js
@@ -0,0 +1,119 @@
+import React, { useState, useEffect } from "react";
+import TextField from "@mui/material/TextField";
+import CircularProgress from "@mui/material/CircularProgress";
+import TableContainerComponent from "../Common/TableContainerComponent";
+import axios from "axios";
+
+function CustomersComponent() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [customers, setCustomers] = useState([]);
+  const [searchedCustomers, setSearchedCustomers] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const getCustomersData = async () => {
+      const response = await axios.get(`http://localhost:3001/api/customers/`);
+      setCustomers(
+        response.data.map((customer) => ({ ...customer, id: customer._id }))
+      );
+      setIsLoading(false);
+    };
+    getCustomersData();
+  }, []);
+
+  useEffect(() => {
+    if (searchTerm !== "") {
+      const customersSearched = customers.filter((customer) => {
+        if (
+          customer.firstName.toLowerCase().includes(searchTerm.toLowerCase())
+        ) {
+          return true;
+        }
+        return false;
+      });
+
+      setSearchedCustomers(customersSearched);
+    }
+  }, [searchTerm]);
+
+  const customersColumns = [
+    {
+      field: "Profile Pic",
+      headerName: "Profile Pic",
+      width: 140,
+    },
+    {
+      field: "firstName",
+      headerName: "First name",
+      width: 140,
+    },
+    {
+      field: "lastName",
+      headerName: "Last name",
+      width: 140,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      width: 200,
+    },
+    {
+      field: "phoneNumber",
+      headerName: "Phone No",
+      width: 140,
+    },
+    {
+      field: "isActive",
+      headerName: "Status",
+      width: 120,
+    },
+    // {
+    //   field: "actions",
+    //   headerName: "Actions",
+    //   width: 120,
+    //   renderCell: (params) => {
+    //     return (
+    //       <div
+    //         className="d-flex justify-content-between align-items-center"
+    //         style={{ cursor: "pointer" }}
+    //       >
+    //         <MatEdit index={params.id} />
+    //       </div>
+    //     );
+    //   },
+    // },
+  ];
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <div>
+      {isLoading && (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "50vh",
+          }}
+        >
+          <CircularProgress />{" "}
+        </div>
+      )}
+      {!isLoading && (
+        <TableContainerComponent
+          rows={searchTerm !== "" ? searchedCustomers : customers}
+          columns={customersColumns}
+          handleSearch={handleSearch}
+          placeholderText={`Search (First Name, Last Name, Email)`}
+        />
+      )}
+    </div>
+  );
+}
+
+export default CustomersComponent;
