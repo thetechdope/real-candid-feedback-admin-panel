bbfaf2b63ceb2e3bf07f66421bd5dc2c79011ae6
Manisha More <manisha.more@otssolutions.com>
2023-02-23 12:40:01 +0530

    login added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import React ,{useEffect} from "react";
-import { Route, Routes ,useNavigate} from "react-router-dom";
+import React, { useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Dashboard from "./components/Dashbord/Dashboard";
 import Sidebar from "./components/Navigation/sidebar/Sidebar.js";
 import Login from "./components/LoginRegister";
@@ -10,36 +10,41 @@ import FeedbackComponent from "./components/Common/FeedbackComponent";
 import CustomersComponent from "./components/Customers/CustomersComponent";
 import BusinessesComponent from "./components/Businesses/BusinessesComponent";
 
-
 function App() {
   const navigate = useNavigate();
-	const auth = localStorage.getItem("loggedIn");
-	useEffect(() => {
-		!auth && navigate("/login");
-	}, [auth]);
+  const auth = localStorage.getItem("loggedIn");
+  useEffect(() => {
+    !auth && navigate("/login");
+  }, [auth]);
   return (
     <>
-			{auth ? (
-				<>
-					<Sidebar>
-						<Routes>
-							<Route path="/" element={<Dashboard />} />
-							<Route path="/changepassword" element={<ChangePassword />} />
-							<Route path="/profile" element={<ProfileUpdate />} />
-							<Route path="/feedback/customer/:email" element={<FeedbackComponent />} />
-							<Route path="/feedback/business/:email" element={<FeedbackComponent />} />
-							<Route path="/customers" element={<CustomersComponent />} />
-							<Route path="/businesses" element={<BusinessesComponent />} />
-							<Route path="/allfeedback" element={<FeedbackComponent />} />
-						</Routes>
-					</Sidebar>
-				</>
-			) : (
-				<Routes>
-					<Route exact path="/login" element={<Login />} />
-				</Routes>
-			)}
-		</>
+      {auth ? (
+        <>
+          <Sidebar>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/changepassword" element={<ChangePassword />} />
+              <Route path="/profile" element={<ProfileUpdate />} />
+              <Route
+                path="/feedback/customer/:email"
+                element={<FeedbackComponent />}
+              />
+              <Route
+                path="/feedback/business/:email"
+                element={<FeedbackComponent />}
+              />
+              <Route path="/customers" element={<CustomersComponent />} />
+              <Route path="/businesses" element={<BusinessesComponent />} />
+              <Route path="/allfeedback" element={<FeedbackComponent />} />
+            </Routes>
+          </Sidebar>
+        </>
+      ) : (
+        <Routes>
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      )}
+    </>
   );
 }
 
